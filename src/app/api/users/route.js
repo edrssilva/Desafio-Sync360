1907f623d89f26cf0596547dd813fdb46442e843
EduardoRSSilva <jeduardo.rss@gmail.com>
2025-07-08 11:53:08 -0300

    Update route.js
    Added PUT and DELETE methods to route.js

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -49,3 +49,64 @@ export async function POST(request) {
     status: 201,
   });
 }
+
+export async function PUT(request) {
+  const connection = await createConnection();
+  const { searchParams } = new URL(request.url);
+  const id = searchParams.get("id");
+
+  const {
+    profile_image_url,
+    first_name,
+    last_name,
+    birth_date,
+    street,
+    neighborhood,
+    city,
+    state,
+    biography,
+  } = await request.json();
+
+  const [result] = await connection.query(
+    `UPDATE users SET 
+      profile_image_url = ?, first_name = ?, last_name = ?, birth_date = ?,
+      street = ?, neighborhood = ?, city = ?, state = ?, biography = ?
+     WHERE id = ?`,
+    [
+      profile_image_url || null,
+      first_name,
+      last_name,
+      birth_date,
+      street,
+      neighborhood,
+      city,
+      state,
+      biography || null,
+      id,
+    ]
+  );
+
+  return new NextResponse(
+    JSON.stringify({ affectedRows: result.affectedRows }),
+    {
+      status: 200,
+    }
+  );
+}
+
+export async function DELETE(request) {
+  const connection = await createConnection();
+  const { searchParams } = new URL(request.url);
+  const id = searchParams.get("id");
+
+  const [result] = await connection.query(`DELETE FROM users WHERE id = ?`, [
+    id,
+  ]);
+
+  return new NextResponse(
+    JSON.stringify({ deleted: result.affectedRows > 0 }),
+    {
+      status: 200,
+    }
+  );
+}
